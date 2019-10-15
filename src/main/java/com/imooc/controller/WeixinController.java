71e42e13e48fd513b511d39374b4d91c5d7179bf
yangxin <1393003255@qq.com>
2019-10-15 17:50:13 +0800

    feat: 获取access_token

--- src/main/java/com/imooc/controller/WeixinController.java
+++ src/main/java/com/imooc/controller/WeixinController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * 微信Controller
@@ -22,5 +23,10 @@ public class WeixinController {
     @GetMapping("/auth")
     public void auth(@RequestParam("code") String code) {
         log.info("code: [{}]", code);
+
+        String url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxb9b8ef133831adbe&secret=22d95c1f0751d30e9f1919e674e6173c&code=" + code + "&grant_type=authorization_code";
+        RestTemplate restTemplate = new RestTemplate();
+        String response = restTemplate.getForObject(url, String.class);
+        log.info("response: [{}]", response);
     }
 }

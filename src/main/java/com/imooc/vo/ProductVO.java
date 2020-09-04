c3c65c641f76f8c7cb78bbf89f59229f78df5087
yangxin <1393003255@qq.com>
2020-09-04 11:26:59 +0800

    style: 增加一行空行

--- src/main/java/com/imooc/vo/ProductVO.java
+++ src/main/java/com/imooc/vo/ProductVO.java
@@ -16,6 +16,7 @@ import java.util.List;
 @Data
 @Builder
 public class ProductVO implements Serializable {
+
     private static final long serialVersionUID = -3974213702722475854L;
 
     @JsonProperty("name")

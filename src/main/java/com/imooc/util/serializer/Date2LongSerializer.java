17539624bc2684b0517bcabd1c133b8e33fcbaa2
yangxin <1393003255@qq.com>
2021-10-20 16:10:13 +0800

    style: 增加一些注释

--- src/main/java/com/imooc/util/serializer/Date2LongSerializer.java
+++ src/main/java/com/imooc/util/serializer/Date2LongSerializer.java
@@ -16,7 +16,8 @@ import java.util.Date;
 public class Date2LongSerializer extends JsonSerializer<Date> {
 
     @Override
-    public void serialize(Date date, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+    public void serialize(Date date, JsonGenerator jsonGenerator, SerializerProvider serializerProvider)
+            throws IOException {
         jsonGenerator.writeNumber(date.getTime() / 1000);
     }
 }
